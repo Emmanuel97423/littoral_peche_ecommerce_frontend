9ed50183d809fe94465ef98ca2b58940ae665ee6
Emmanuel Narcisse <epokweb@hotmail.fr>
2023-02-24 17:08:56 +0400

    Translate empty cart

--- src/modules/cart/components/empty-cart-message/index.tsx
+++ src/modules/cart/components/empty-cart-message/index.tsx
@@ -3,13 +3,13 @@ import UnderlineLink from "@modules/common/components/underline-link"
 const EmptyCartMessage = () => {
   return (
     <div className="bg-amber-100 px-8 py-24 flex flex-col justify-center items-center text-center">
-      <h1 className="text-2xl-semi">Your shopping bag is empty</h1>
+      <h1 className="text-2xl-semi">Votre panier est vide</h1>
       <p className="text-base-regular mt-4 mb-6 max-w-[32rem]">
-        You don&apos;t have anything in your bag. Let&apos;s change that, use
-        the link below to start browsing our products.
+        Vous n&apos;avez rien dans votre panier. Changeons cela, utilisons
+        le lien ci-dessous pour commencer à parcourir nos articles.
       </p>
       <div>
-        <UnderlineLink href="/store">Explore products</UnderlineLink>
+        <UnderlineLink href="/store">Explorer plus d&apos;articles</UnderlineLink>
       </div>
     </div>
   )
